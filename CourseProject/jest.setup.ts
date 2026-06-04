dcd59105dcb0311eec76e0a7508dc3836f8ea5d8
Glazochek <an9680411@gmail.com>
2026-06-04 19:22:09 +0700

    fixing more

--- CourseProject/jest.setup.ts
+++ CourseProject/jest.setup.ts
@@ -1,5 +1,45 @@
 import "@testing-library/jest-native/extend-expect";
 
+jest.mock("react-native-reanimated", () => {
+  const { View } = require("react-native");
+  return {
+    __esModule: true,
+    default: {
+      View,
+      Text: View,
+      createAnimatedComponent: (c: unknown) => c,
+    },
+    useSharedValue: (init: number) => ({ value: init }),
+    useAnimatedStyle: (fn: () => object) => fn(),
+    withSpring: (to: number) => to,
+  };
+});
+
+jest.mock("react-native-gesture-handler", () => {
+  const React = require("react");
+  const { View } = require("react-native");
+  const pan = () => {
+    const g = {
+      activeOffsetX: () => g,
+      onBegin: () => g,
+      onUpdate: () => g,
+      onEnd: () => g,
+    };
+    return g;
+  };
+  return {
+    GestureHandlerRootView: View,
+    GestureDetector: ({ children }: { children: React.ReactNode }) =>
+      React.createElement(View, null, children),
+    Gesture: { Pan: pan },
+    RectButton: View,
+    Swipeable: View,
+    State: {},
+    PanGestureHandler: View,
+    FlatList: View,
+  };
+});
+
 jest.mock("expo-router", () => {
   const React = require("react");
   const { Text } = require("react-native");
